1dd0434cb602087f96e8adb8e9f897d4017ec65f
ctile <tilechristopher@gmail.com>
2021-09-15 18:01:41 -0400

    2 files modified

--- src/MenuItem.js
+++ src/MenuItem.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function MenuItem(props) {
   // Copy/paste what appears in one of the menu elements, replacing name1 with props.name, price1 with props.price, and description with props.children (children is the content of the HTML element, i.e., what appears between the open and closing tags, here it'll be the description appearing between the <p> and </p>)
-  return '';
+  return  (   
+    <div>
+      <h2>
+        {props.name}, ${props.price}
+      </h2>
+      <p>{props.children}</p>
+    </div>
+  ); 
 }
 
 export default MenuItem;
--- src/index.js
+++ src/index.js
@@ -3,77 +3,20 @@ import ReactDOM from 'react-dom';
 
 import MenuItem from './MenuItem';
 
-// Define the following variables:
-//     * name for name of item assigned to 'House Blend'
-//     * price for price of item assigned to 1.49
-//     * description assigned to 'A smooth, <em>mild</em> blend of
-//     coffees from Mexico, Bolivia and Guatemala.'
-
-// Then replace the definition of menuItemElement1 below to use these variables so the output looks exactly the same:
-
-let name = 'House Blend';
-let price = '1.49';
-let description = 'A smooth, mild blend of coffees from Mexico, Bolivia and Guatemala.';
-
-const menuItemElement1 = (
-  <div>
-    <h2>{name}, ${price}</h2>
-    <p>
-      {description}
-    </p>
-  </div>
-);
-
-// Copy/paste/modify the variables/definitions for menuItemElement2, menuItemElement3, and menuItemElement4 (BAD IDEA IN GENERAL!).  NOTE: We need to use let initially instead of const if we're changing the values of variables and then NO use of let after that.
-
-
-name = 'Mocha Cafe Latte';
-price = '2.35';
-description = 'Espresso, steamed milk and chocolate syrup.';
-
-const menuItemElement2 = (
-  <div>
-    <h2>{name}, ${price}</h2>
-    <p>
-      {description}
-    </p>
-  </div>
-);
-
-name = 'Cappuccino';
-price = '1.89';
-description = 'A mixture of espresso, steamed milk and foam.';
-
-const menuItemElement3 = (
-  <div>
-    <h2>{name}, ${price}</h2>
-    <p>
-      {description}
-    </p>
-  </div>
-);
-
-name = 'Chai Tea';
-price = '1.85';
-description = 'A spicy drink made with black tea, spices, milk and honey.';
-
-const menuItemElement4 = (
-  <div>
-    <h2>{name}, ${price}</h2>
-    <p>
-      {description}
-    </p>
-  </div>
-);
-
-// After you do this, add menuItemElement2, menuItemElement3, and menuItemElement4 in the definition of the menu element:
-
 const menuElement = (
   <div>
-    {menuItemElement1}
-    {menuItemElement2}
-    {menuItemElement3}
-    {menuItemElement4}
+    <MenuItem name = 'House Blend' price = {1.49}>
+    A smooth, mild blend of coffees from Mexico, Bolivia and Guatemala.
+    </MenuItem>
+    <MenuItem name = 'Mocha Cafe Latte' price = {2.35}>
+    Espresso, steamed milk and chocolate syrup.
+    </MenuItem>
+    <MenuItem name = 'Cappuccino' price = {1.89}>
+    A mixture of espresso, steamed milk and foam.
+    </MenuItem>
+    <MenuItem name = 'Chai Tea' price = {1.85}>
+    A spicy drink made with black tea, spices, milk and honey.
+    </MenuItem>
   </div>
 );
 
